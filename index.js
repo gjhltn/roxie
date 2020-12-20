7ceee97d80232ca4b826243b30cd0c4873d178f2
g j h <guy@swirrl.com>
2020-12-20 23:27:12 +0000

    add testing

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const path = require('path');
 const fs = require('fs');
+const inquirer = require('inquirer');
+const _ = require('lodash');
+
+//
 
 const adapter = new FileSync('db.json')
 const db = low(adapter)
@@ -57,14 +61,39 @@ updateAll = (dataSource) => {
 		.write()
 }
 
+const interactiveUpdate = () => {
+  const questions = [
+    {
+      type: "list",
+      name: "action",
+      message: "What do you want to do?",
+      choices: [
+        { name: "action 1", value: "Action1" },
+        { name: "action 2", value: "Action2" },
+        { name: "Exit program", value: "quit"}
+      ]
+    }
+  ];
+  return inquirer.prompt(questions);
+};
+
+const main = async () => {
+	let files = ["wang","chung"]
+	for (const file of files) {
+		await interactiveUpdate()
+    		.then(answers => {
+				console.log(answers)
+			})
+		}
+}
+
 const buildUpdate = item => {
 	const fileName = item.file
 	const [author,title] = fileName.split('#')
 	const [surname,forename] = author.split(',')
 	return {
 		file: fileName,
-		surname: surname.trim(),
-		forename: forename.trim(),
+		author: author,
 		title: title.trim()
 	}
 }
@@ -73,7 +102,64 @@ const buildUpdate = item => {
 
 const run = (directory,dataSource) => {
 	sync(directory,dataSource)
-	updateAll(dataSource)
 }
 
+/*
 run('./files/',db)
+main()
+*/
+
+/* output */
+
+var compiled = _.template(`
+	<%_.forEach(authors, function(name) { %>
+		<%- user %>
+	<% });%>`
+)
+
+const humaniseArray = (arr) =>
+	switch arr.length
+
+const authors = item =>
+	item.authors.map(name=>"x")
+
+const render = (item) => {
+	const result = 
+		`
+			${authors(item)}
+		`
+	
+	
+	console.log(result)
+}
+
+const singleAuthor = 
+	{
+		authors: [
+			{
+				lastName:'Flintstone',
+				firstName:'Fred'
+			}
+		]
+	}
+
+const twoAuthors =
+		{
+		authors: [
+			{
+				lastName:'Flintstone',
+				firstName:'Fred'
+			},
+			{	
+				lastName: 'Rubble',
+				firstName: 'Barney'
+			}
+		]
+	}
+
+render(singleAuthor)
+render(twoAuthors)
+
+/*
+	“Author’s Last Name, Author’s First Name. Title of Book: Subtitle of Book. Place of Publication: Publisher’s Name, Date of Publication.”
+*/
