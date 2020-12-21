42e7b9b4e88925fd1626939c7a7fe9ba425fbc2d
g j h <guy@swirrl.com>
2020-12-21 01:10:14 +0000

    rendering functions

--- chicago.js
+++ chicago.js
@@ -1,5 +1,23 @@
-export const humaniseArray = (arr) =>
-	arr.join(", ")
+export const oxford = (arr, conjunction, ifempty) => {
+    const l = arr.length;
+    if (!l) return ifempty;
+    if (l<2) return arr[0];
+    if (l<3) return arr.join(` ${conjunction} `);
+    arr = arr.slice();
+    arr[l-1] = `${conjunction} ${arr[l-1]}`;
+    return arr.join(", ");
+}
 
-export const authors = item =>
-	item.authors.map(name=>"x")
+export const nameLastFirst = name =>`${name.last}, ${name.first}`
+	
+export const nameFirstLast = name =>`${name.first} ${name.last}`
+
+export const humaniseArray = (arr) => oxford(arr, "and", "")
+
+export const names = arr => {
+    const l = arr.length
+    if (!l) return
+	let flat = [nameLastFirst(arr[0])]
+	if (l>1) flat = flat.concat(arr.slice(1).map(name=>nameFirstLast(name)))
+	return humaniseArray(flat)
+}
--- chicago.test.js
+++ chicago.test.js
@@ -5,6 +5,45 @@ describe('humaniseArray', () => {
 		expect(c.humaniseArray(["a"])).toEqual("a")
 	})
 	it('humanises 2 items', () => {
-		expect(c.humaniseArray(["a"])).toEqual("a, and b")
+		expect(c.humaniseArray(["a","b"])).toEqual("a and b")
+	})
+	it('humanises 3 items', () => {
+		expect(c.humaniseArray(["a","b","c"])).toEqual("a, b, and c")
+	})
+	it('humanises 4 items', () => {
+		expect(c.humaniseArray(["a","b","c","d"])).toEqual("a, b, c, and d")
+	})
+})
+
+describe('name', () => {
+	it('last, first', () => {
+		expect(c.nameLastFirst({last: "Wittgenstein",first: "Ludwig"})).toEqual("Wittgenstein, Ludwig")
+	})
+	it('first, last', () => {
+		expect(c.nameFirstLast({last: "Wittgenstein",first: "Ludwig"})).toEqual("Ludwig Wittgenstein")
+	})
+})
+
+describe('names', () => {
+	it('one name', () => {
+		expect(c.names([{last: "Wittgenstein",first: "Ludwig"}])).toEqual("Wittgenstein, Ludwig")
+	})
+	it('two names', () => {
+		expect(c.names(
+			[
+				{last: "Wittgenstein",first: "Ludwig"},
+				{last: "Dodd", first:"Ken"}
+			]
+		)).toEqual("Wittgenstein, Ludwig and Ken Dodd")
+	})
+	it('four names', () => {
+		expect(c.names(
+			[
+				{last: "Asheton",first: "Scott"},
+				{last: "Asheton", first:"Ron"},
+				{last: "Williamson", first:"James"},
+				{last: "Pop", first:"Iggy"},
+			]
+		)).toEqual("Asheton, Scott, Ron Asheton, James Williamson, and Iggy Pop")
 	})
 })
