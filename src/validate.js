6270381a542cd23d9e4e786e2151e20b418a0d6a
g j h <guy@swirrl.com>
2020-12-23 13:39:49 +0000

    linting

--- src/validate.js
+++ src/validate.js
@@ -1,11 +1,22 @@
-import * as v from './validate.js';
-
 export const check = (item) => {
 	const fail = [false,"Invalid type"]
 	if (!item || !item.type) return fail
 	switch(item.type) {
-		case "book": return true
+		case "book": return book(item)
+		case "chapter": return chapter(item)
+		case "article": return article(item)
 		default: return fail
 	}
-	return true
+}
+
+export const book = (item) => {
+	return [false,"Invalid book"]
+}
+
+export const chapter = (item) => {
+	return [false,"Invalid chapter"]
+}
+
+export const article = (item) => {
+	return [false,"Invalid article"]
 }
