7d581a17c84e130cc85326bcd7cb130cc338f4ad
g j h <guy@swirrl.com>
2020-12-26 04:42:48 +0000

    begin crud

--- api/routes/items.js
+++ api/routes/items.js
@@ -1,5 +1,11 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const shortid = require('shortid');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
 
 const items=[
 	{
@@ -38,8 +44,64 @@ const items=[
 }
 ]
 
-router.get("/", function(req, res, next) {
-    res.send(items);
+router.get("/", async function(req, res, next) {
+	const result = db.get('items').value();
+	return res.status(200).send(result);
 });
+
+router.get("/:id", async function(req, res, next) {
+	const { id } = req.params;
+	const result = db.get('items').find({ id:id }).value();
+	return res.status(200).send(result);
+});
+
+router.delete(`/:id`, async function(req, res) {
+    const { id } = req.params;
+    console.log(id);
+
+    db.get('items')
+      .remove({ id })
+      .write()
+
+    return res.status(202).send({
+      error: false
+    })
+})
+
+router.post(`/new`, async function(req, res) {
+	const { title } = req.body;
+    const id = shortid.generate();
+    const items = db
+      .get('items')
+      .push({ id, title })
+      .write();
+
+    const item = db.get('items')
+      .find({ id })
+      .value();
+
+    return res.status(201).send({
+      error: false,
+      item
+  });
+})
+
+router.put(`/update`, async function(req, res) {
+	const { title, id } = req.body;
+
+    let items = db.get('items')
+        .find({ id })
+        .assign({ title })
+        .write();
+
+    const item = db.get('items')
+      .find({ id })
+      .value();
+
+    return res.status(202).send({
+      error: false,
+      item
+  });
+})
 
 module.exports = router;
