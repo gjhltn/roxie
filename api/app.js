3c3d83723e175c90829901368db0bea0e1999866
g j h <guy@swirrl.com>
2020-12-27 12:54:46 +0000

    address regressiins in api

--- api/app.js
+++ api/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var testAPIRouter = require("./routes/testAPI");
 var itemsRouter = require("./routes/items");
 var cors = require("cors");
 
