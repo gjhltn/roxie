60cc9878b315824ebdc635acd716698adce13411
g j h <guy@swirrl.com>
2020-12-29 15:58:48 +0000

    placeholder journal

---
+++ client/src/sources/journal/source.js
@@ -0,0 +1,15 @@
+// JOURNAL
+
+import {
+	authorship,
+	editors,
+	translators,
+	imprint,
+	names
+} from '../citation.js';
+
+export const bibliography = (item) =>
+	`TODO`
+
+export const note = (item) =>
+	`TODO`
--- client/src/sources/source.js
+++ client/src/sources/source.js
@@ -1,10 +1,12 @@
 import * as book from './book/source.js';
 import * as chapter from './chapter/source.js';
+import * as journal from './journal/source.js';
 
 export const bibliography = (item) => {
 	switch (item.type) {
 		case 'book': return book.bibliography(item)
 		case 'chapter': return chapter.bibliography(item)
+		case 'journal': return journal.bibliography(item)
 		default: return `[unsupported item type: ${item.type}]`
 	}
 }
@@ -13,6 +15,7 @@ export const note = (item) => {
 	switch (item.type) {
 		case 'book': return book.note(item)
 		case 'chapter': return chapter.note(item)
+		case 'journal': return journal.note(item)
 		default: return `[unsupported item type: ${item.type}]`
 	}
 }
