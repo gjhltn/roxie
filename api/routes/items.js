58048e0d6dffbd44c9532e5b6cd9c714136c854a
g j h <guy@swirrl.com>
2021-01-03 06:49:24 +0000

    fox refression

--- api/routes/items.js
+++ api/routes/items.js
@@ -5,7 +5,7 @@ const FileSync = require('lowdb/adapters/FileSync');
 const shortid = require('shortid');
 const fs = require('fs');
 
-const dbFile = "devdb.json"
+const dbFile = "db.json"
 
 const backup = `${dbFile}.backup.${new Date().toISOString().replace(/:/g,"-")}`
 
