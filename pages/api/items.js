3fcde467507eb98283d1d7f015eec6f2b1012b89
g j h <guy@swirrl.com>
2022-04-15 11:18:35 +0100

    begin low

--- pages/api/items.js
+++ pages/api/items.js
@@ -4,27 +4,33 @@ import { fileURLToPath } from 'url'
 import lodash from 'lodash'
 import lodashId from 'lodash-id'
 
-// see https://stackoverflow.com/questions/65920839/return-to-parent-object-in-lodash-chaining-chain
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'db.json')
 
 class LowWithLodash extends Low {
   chain = lodash.chain(this).get('data')
 }
 
-const adapter = new JSONFile('db.json')
+const adapter = new JSONFile(file)
 const db = new LowWithLodash(adapter)
 lodash.mixin(lodashId);
 
-const users = [{
-	id: 1
-}, {
-	id: 2
-}, {
-	id: 3
-}]
+const defaultContents = {
+	items: [
+		{id: 1, name: "item one"}
+	],
+	collections: [
+		{id: 1, name: "collection one"}
+	]
+}
 
 export default async function handler(req, res) {
 	await db.read()
-  // Get data from your database
-  res.status(200).json(users)
+	db.data ||= defaultContents
+  res.status(200).json(db.data.items)
+  db.data.items.push({
+	  id: 2,
+	 name: 'hello world'
+	 })
+  await db.write()
 }
