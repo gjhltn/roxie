da47807bacaeaafd373af39de4ae97a614b7385c
g j h <guy@swirrl.com>
2022-05-07 05:41:23 +0100

    journal

--- pages/api/downloads/index.js
+++ pages/api/downloads/index.js
@@ -2,7 +2,7 @@ import { Document, Packer, Paragraph, TextRun } from 'docx'
 import contentDisposition from 'content-disposition'
 import { read } from '/db/items'
 import sortByNameAndYear from '/helpers/sortByNameAndYear'
-import { authorship, editors, translators, imprint } from '/helpers/citation'
+import { names, authorship, editors, translators, imprint } from '/helpers/citation'
 
 // http://localhost:3000/api/downloads
 
@@ -23,10 +23,56 @@ const docxBibliographyBook = (item, elideAuthor) =>
 		]
 	})
 
-const docxBibliographyJournal = (item, elideAuthor) =>
-	new Paragraph({
-		children: [new TextRun('Lorem Ipsum Foo Bar'), new TextRun('Hello World')]
+const docxBibliographyJournal = (item, elideAuthor) => {
+	let children = [
+		new TextRun(names(item.authors, { flipFirst: true })),
+		new TextRun('. '),
+		new TextRun('"'),
+		new TextRun(item.title),
+		new TextRun('." '),
+		new TextRun({
+			italics: true,
+			text: item.journal
+		}),
+		new TextRun(' ')
+	]
+	if (item.volume) {
+		children.push(new TextRun(item.volume))
+		if (item.issue) {
+			children.push(new TextRun(', '))
+		}
+	}
+	if (item.issue) {
+		children.push(new TextRun('No. '))
+		children.push(new TextRun(item.issue))
+		children.push(new TextRun(' '))
+	}
+	children.push(new TextRun('('))
+	if (item.date) {
+		children.push(new TextRun(item.date))
+	}
+	if (item.date && item.year) {
+		children.push(new TextRun(' '))
+	}
+	if (item.year) {
+		children.push(new TextRun(item.year))
+	}
+	children.push(new TextRun(')'))
+	if (item.location) {
+		children.push(new TextRun(': '))
+		children.push(new TextRun(item.location))
+	}
+	children.push(new TextRun('.'))
+	if (item.url) {
+		children.push(new TextRun(' '))
+		children.push(new TextRun(item.url))
+		children.push(new TextRun('.'))
+	}
+	return new Paragraph({
+		style: 'hung',
+		children: children
 	})
+}
 
 const docxBibliographyChapter = (item, elideAuthor) =>
 	new Paragraph({
@@ -42,7 +88,7 @@ const render = (item, previousItem) => {
 			paragraph = docxBibliographyBook(item, elideAuthor)
 			break
 		case 'journal':
-			paragraph = null // docxBibliographyJournal(item, elideAuthor)
+			paragraph = docxBibliographyJournal(item, elideAuthor)
 			break
 		case 'chapter':
 			paragraph = null // docxBibliographyChapter(item, elideAuthor)
@@ -74,14 +120,14 @@ const buildDoc = data => {
 					next: 'Normal',
 					quickFormat: true,
 					run: {
-						font: "Helvetica",
+						font: 'Helvetica',
 						size: 28
 					},
 					paragraph: {
 						indent: {
-			left: 0,
-			hanging: 720
-		},
+							left: 0,
+							hanging: 720
+						}
 					}
 				}
 			]
